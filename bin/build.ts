68fc30ef949ac246fa78ee8e518a90d3c1bb6d99
李欢 <li.huan@synyi.com>
2019-12-26 14:55:32 +0800

    change version

--- bin/build.ts
+++ bin/build.ts
@@ -2,28 +2,72 @@ import shell, { ShellString } from 'shelljs';
 import chalk from 'chalk';
 import path from 'path';
 
-// build ts文件
-const res: ShellString = shell.exec('yarn run tsc');
-if (res.code !== 0) {
-  shell.echo(chalk.red('Error: build failed'));
+function tsc(): void {
+  // build ts文件
+  const res: ShellString = shell.exec('yarn run tsc');
+  if (res.code !== 0) {
+    shell.echo(chalk.red('Error: build failed'));
+  }
 }
 
-// 拷贝模板文件
-shell.echo(chalk.green('开始拷贝模板文件!'));
-const sourcePath = path.join(__dirname, '../packages/templates');
-const targetPath = path.join(__dirname, '../dist/packages/templates/');
+function copyTemplates(): void {
+  // 拷贝模板文件
+  shell.echo(chalk.green('开始拷贝模板文件!'));
+  const sourcePath = path.join(__dirname, '../packages/templates');
+  const targetPath = path.join(__dirname, '../dist/packages/templates/');
 
-shell.mkdir('-p', targetPath);
+  shell.mkdir('-p', targetPath);
 
-const templateFiles = shell.find(sourcePath)
-  .filter((file) => file.match(/.template$/));
+  const templateFiles = shell.find(sourcePath)
+    .filter((file) => file.match(/.template$/));
 
-shell.cp('-Rf', templateFiles, path.join(targetPath));
+  shell.cp('-Rf', templateFiles, path.join(targetPath));
+}
 
-// 添加执行权限
+function createPackageJson(): void {
+  // 新建package.json
+  const keys: string[] = [
+    'name',
+    'version',
+    'main',
+    'license',
+    'author',
+    'description',
+    'bin',
+    'keywords',
+    'dependencies',
+  ];
+  const packageJsonFile = path.join(__dirname, '../package.json');
+  const packageJsonString = shell.cat(packageJsonFile);
+  const packageJson = JSON.parse(packageJsonString);
+  const newJson: any = {};
+  keys.forEach((key) => {
+    if (key === 'main') {
+      newJson[key] = './bin/index.js';
+    } else if (key === 'bin') {
+      newJson[key] = packageJson[key];
+      newJson[key].twify = './bin/index.js';
+    } else {
+      newJson[key] = packageJson[key];
+    }
+  });
+  new ShellString(JSON.stringify(newJson, null, 2))
+    .to(path.join(__dirname, '../dist/package.json'));
+}
 
-const binPath = path.join(__dirname, '../dist/bin');
-const jsFile = shell.find(binPath);
-jsFile.forEach((file) => {
-  shell.chmod('+x', file);
-});
+function addExcutableAuth(): void {
+  // 添加执行权限
+  const binPath = path.join(__dirname, '../dist/bin');
+  const jsFile = shell.find(binPath);
+  jsFile.forEach((file) => {
+    shell.chmod('+x', file);
+  });
+}
+
+((): void => {
+  shell.rm('-rf', path.join(__dirname, '../dist'));
+  tsc();
+  copyTemplates();
+  createPackageJson();
+  addExcutableAuth();
+})();
--- dist/packages/commands/remove.js
+++ dist/packages/commands/remove.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var shelljs_1 = __importDefault(require("shelljs"));
 var path_1 = __importDefault(require("path"));
-var removeShell = "\n  yarn remove tailwindcss -S\n  yarn remove postcss-import -D\n  yarn remove postcss-loader -D\n  yarn remove postcss-scss -D\n  yarn remove @angular-builders/custom-webpack -S\n  yarn remove @fullhuman/postcss-purgecss -D\n";
+var removeShell = "\n  yarn remove tailwindcss\n  yarn remove postcss-import\n  yarn remove postcss-loader\n  yarn remove postcss-scss\n  yarn remove autoprefixer\n  yarn remove @angular-builders/custom-webpack\n  yarn remove @fullhuman/postcss-purgecss\n";
 function uninstallDependencies() {
     var res = shelljs_1.default.exec(removeShell);
     if (res.code !== 0) {
